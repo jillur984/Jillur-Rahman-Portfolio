13b874223d912bda3a2d88dfc288e706d32b5fcf
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-09-09 11:37:44 +0600

    Jillur Rahman Portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,33 +21,35 @@ const Navbar = () => {
 
       <ul
         className={`${
-          menu ? "block" : "hidden"
-        } mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
+          menu
+            ? "fixed top-20 right-10 bg-green-300 text-black shadow-md rounded-lg p-4 flex flex-col items-center space-y-3 z-50 w-48"
+            : "hidden"
+        } md:static md:flex md:flex-row md:items-center md:space-x-8 font-semibold`}
       >
         <a href="#About" onClick={closeMenu}>
-          <li className="text-md transition-all duration-300 p-1 md:p-0">About</li>
+          <li className="text-md transition-all duration-300 p-2 md:p-0 hover:bg-blue-600 rounded-md">About</li>
         </a>
         <a href="#Experience" onClick={closeMenu}>
-          <li className="text-md transition-all duration-300 p-1 md:p-0">Experience</li>
+          <li className="text-md transition-all duration-300 p-2 md:p-0 hover:bg-blue-600 rounded-md">Experience</li>
         </a>
         <a href="#Projects" onClick={closeMenu}>
-          <li className="text-md transition-all duration-300 p-1 md:p-0">Projects</li>
+          <li className="text-md transition-all duration-300 p-2 md:p-0 hover:bg-blue-600 rounded-md">Projects</li>
         </a>
         <a href="#Footer" onClick={closeMenu}>
-          <li className="text-md transition-all duration-300 p-1 md:p-0">Contact</li>
+          <li className="text-md transition-all duration-300 p-2 md:p-0 hover:bg-blue-600 rounded-md">Contact</li>
         </a>
       </ul>
 
       {showMenu ? (
         <RiMenu2Line
           size={30}
-          className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          className="md:hidden absolute right-10 top-6 transition-all duration-300 cursor-pointer"
           onClick={toggleMenu}
         />
       ) : (
         <RiCloseLine
           size={30}
-          className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          className="md:hidden absolute right-10 top-6 transition-all duration-300 cursor-pointer"
           onClick={closeMenu}
         />
       )}
